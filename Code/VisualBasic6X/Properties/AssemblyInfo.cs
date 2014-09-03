5899fdf97db993d39609b08e7ae158dbcf79ff19
David Moore <david@sadrobot.co.nz>
2014-09-04 06:55:31 +1200

    Fixed missing versioning includes

--- Code/VisualBasic6X/Properties/AssemblyInfo.cs
+++ Code/VisualBasic6X/Properties/AssemblyInfo.cs
@@ -3,7 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Visual Basic 6 Integration for Visual Studio")]
 [assembly: AssemblyDescription("Extends Visual Studio to support Visual Basic 6 projects.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid("28445F67-AF98-483E-A6EE-E781A6E663DE")]
